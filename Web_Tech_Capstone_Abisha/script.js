b92281d37e6961665b77dd9430dc3c806b0f2a2c
Abijm <95025374+Abijm@users.noreply.github.com>
2021-12-15 21:07:29 +0530

    Add files via upload
    https://timelyregalbrowsers.abishajebamalar.repl.co/

---
+++ Web_Tech_Capstone_Abisha/script.js
@@ -0,0 +1,43 @@
+
+function colorchange()
+{
+   document.getElementById('id1').style.backgroundColor='#126fb0';
+   document.getElementById('id1').style.color="white";
+   
+  }
+  function submit_form()
+  {
+    var name1=document.getElementById('name').value;
+    alert("Hi "+ name1+ "\n You do not own a dog.\n You have a dog.\n And the dog has you.");
+  }
+  function bark1()
+  {
+       var audio = document.getElementById("audio1");
+       audio.play();
+    
+  }
+  function bark2()
+  {
+       var audio = document.getElementById("audio2");
+       audio.play();
+    
+  }
+  function bark3()
+  {
+       var audio = document.getElementById("audio3");
+       audio.play();
+    
+  }
+  function bark4()
+  {
+       var audio = document.getElementById("audio4");
+       audio.play();
+    
+  }
+  function bark5()
+  {
+       var audio = document.getElementById("audio5");
+       audio.play();
+    
+  }
+  
